19d353c68dd818bbd94fe64c1dcb3942e19e0cac
oliverjpaulinop <oliverjpaulinop@gmail.com>
2026-04-10 17:35:13 -0400

    agregando apartado de login basico

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,27 @@
 package org.example;
+
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import org.example.services.MongoService;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-      
+    public static void main(String[] args) {
+
         MongoService.getInstance();
-         int httpPort = 7770;
+        int httpPort = 7770;
         Javalin app = Javalin.create(config -> {
             // Servir archivos estáticos del frontend desde el classpath
             config.staticFiles.add("/static", Location.CLASSPATH);
-
             // Habilitar CORS para clientes REST/gRPC externos
             config.bundledPlugins.enableCors(cors -> {
                 cors.addRule(rule -> {
-                    rule.anyHost();
+                    rule.reflectClientOrigin = true;
                     rule.allowCredentials = true;
                 });
             });
         }).start(httpPort);
+
+        app.get("/", ctx -> ctx.redirect("/login.html"));
+
     }
 }
--- src/main/java/org/example/services/MongoService.java
+++ src/main/java/org/example/services/MongoService.java
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import java.lang.String;
 
 
 public class MongoService {
